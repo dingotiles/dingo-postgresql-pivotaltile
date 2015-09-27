628ddd81c2a6111f11ecc9fd927e28c11ae617f6
Dr Nic Williams <drnicwilliams@gmail.com>
2015-09-27 13:05:50 -0700

    initial generate-product script

---
+++ ci/tasks/generate-product.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+docker_version=$(cat docker-boshrelease/version)
+cp -r docker-boshrelease/release.tgz tile/releases/docker-release-$(docker_version).tgz
+
+pg_docker_version=$(cat postgresql-docker-boshrelease/version)
+cp -r postgresql-docker-boshrelease/release.tgz tile/releases/postgresql-docker-release-$(pg_docker_version).tgz
+
+cd tile
+ls -al releases/
