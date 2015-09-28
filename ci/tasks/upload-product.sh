e489b03c7473ce71b7ea4c4c2139cbd07113bf70
Dr Nic Williams <drnicwilliams@gmail.com>
2015-09-28 12:08:58 -0700

    upload product via api

--- ci/tasks/upload-product.sh
+++ ci/tasks/upload-product.sh
@@ -1,3 +1,23 @@
 #!/bin/bash
 
+set -x # print commands
+set -e # fail fast
+
 ls -la generated-tile/*
+tile_path=generated-tile/*.pivotal
+
+if [[ "${opsmgr_skip_ssl_verification}X" != "X" ]]; then
+  skip_ssl="-k "
+fi
+
+function _curl() {
+  curl ${skip_ssl} -u ${opsmgr_username}:${opsmgr_password} $@
+}
+
+_curl "${opsmgr_url}/api/api_version"
+
+# there is no way to delete a specific product (the one being uploaded)
+# so delete all products and hope for the best
+_curl "${opsmgr_url}/api/products" -d '' -X DELETE
+
+_curl "${opsmgr_url}/api/products" -X POST -F "product[file]=@${tile_path}"
