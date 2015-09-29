037fe0b871f31b2df0a1846405c9d12bce195d0c
Dr Nic Williams <drnicwilliams@gmail.com>
2015-09-28 19:56:54 -0700

    update installation-settings with network az settings

--- ci/tasks/upload-product.sh
+++ ci/tasks/upload-product.sh
@@ -81,7 +81,9 @@ echo Adding the product to the installation
 curl -f -v ${skip_ssl} -u ${opsmgr_username}:${opsmgr_password} \
   ${opsmgr_url}/api/installation_settings/products -X POST \
     -d "name=postgresql-docker&product_version=${product_version}"
+echo
 
+echo Setting AZ for installation to same as microbosh
 microbosh_az_id=$(curl -sf ${skip_ssl} -u ${opsmgr_username}:${opsmgr_password} \
   ${opsmgr_url}/api/installation_settings | jq -r ".products[0].singleton_availability_zone_reference")
 # add following to installation_settings (from microbosh)
@@ -95,7 +97,12 @@ curl -sf ${skip_ssl} -u ${opsmgr_username}:${opsmgr_password} \
   ${opsmgr_url}/api/installation_settings | \
   jq ".products[1].singleton_availability_zone_reference = \"${microbosh_az_id}\" | .products[1].availability_zone_references = [\"${microbosh_az_id}\"]" \
   > installation_settings.json
+echo
 
-cat installation_settings.json
+curl -sf -v ${skip_ssl} -u ${opsmgr_username}:${opsmgr_password} \
+  ${opsmgr_url}/api/installation_settings -X POST \
+    -F 'installation[file]=@installation_settings.json'
+echo
+
 # start installation /api/installation -X POST
 # poll for status installation /api/installation -X GET
