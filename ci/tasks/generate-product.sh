99ef37c873b195ca98325245ee6ddab7ccc6befb
Dr Nic Williams <drnicwilliams@gmail.com>
2016-03-06 13:00:29 +1000

    spruce merge tile templates

--- ci/tasks/generate-product.sh
+++ ci/tasks/generate-product.sh
@@ -5,9 +5,17 @@ set -x # print commands
 
 TILE_VERSION=$(cat tile-version/number)
 
-mkdir -p product/releases/
+mkdir -p tile/tmp/metadata
+mkdir -p workspace/metadata
+mkdir -p workspace/releases
+mkdir -p workspace/content_migrations
 
-cat >tile/templates/metadata/releases.yml <<YAML
+cat >tile/tmp/metadata/version.yml <<EOF
+---
+product_version: "${TILE_VERSION}"
+EOF
+
+cat >tile/tmp/metadata/releases.yml <<YAML
 ---
 releases:
 YAML
@@ -16,7 +24,7 @@ boshreleases=("patroni-docker" "etcd" "remote-syslog" "broker-registrar")
 for boshrelease in "${boshreleases[@]}"
 do
   release_version=$(cat ${boshrelease}/version)
-  cat >>tile/templates/metadata/releases.yml <<YAML
+  cat >>tile/tmp/metadata/releases.yml <<YAML
   - name: ${boshrelease}
     file: ${boshrelease}-${release_version}.tgz
     version: "${release_version}"
@@ -29,5 +37,21 @@ YAML
   fi
 done
 
-cat tile/templates/metadata/releases.yml
-ls -l product/releases/
+spruce merge \
+  tile/tmp/metadata/version.yml \
+  tile/tmp/metadata/releases.yml \
+  tile/templates/metadata/form_types.yml \
+  tile/templates/metadata/property_blueprints.yml \
+  tile/templates/metadata/job_types.yml \
+  tile/templates/metadata/errands_broker_registrar.yml \
+  tile/templates/deploy_and_delete_errands.yml \
+  tile/templates/metadata/base.yml > workspace/metadata/dingo-postgresql.yml
+
+
+cd workspace
+ls -laR .
+
+echo "creating dingo-postgresql-${TILE_VERSION}.pivotal file"
+zip -r dingo-postgresql-${TILE_VERSION}.pivotal content_migrations metadata releases
+
+mv dingo-postgresql-${TILE_VERSION}.pivotal ../product
