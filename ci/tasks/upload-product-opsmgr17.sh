2df81ecf7b080e949a4af2e5fdca0d254cfddbe8
Dr Nic Williams <drnicwilliams@gmail.com>
2016-08-14 22:59:43 +1000

    fix syntax

--- ci/tasks/upload-product-opsmgr17.sh
+++ ci/tasks/upload-product-opsmgr17.sh
@@ -48,7 +48,7 @@ mv ${tile_path} ${zip_tile_path}
   echo Installing product version $product_version
 mv ${zip_tile_path} ${tile_path}
 
-staged_product_guid=$(curl_auth -s "${opsmgr_url}/api/v0/staged/products" | jq -r ".[] | select(.type == \"dingo-postgresql\")" | .guid)
+staged_product_guid=$(curl_auth -s "${opsmgr_url}/api/v0/staged/products" | jq -r ".[] | select(.type == \"dingo-postgresql\") | .guid")
 
 if [[ "${already_staged}X" == "X" ]]; then
   echo Adding product ${product_version} to the installation
